09bbcf6b5408408ea7fbe0a5982e7b6caea74b9c
白雅雯 <baiyawen@bytedance.com>
2019-07-29 20:34:17 +0800

    add PlayChess

---
+++ PlayChess/src/MyChessAndGoGame.java
@@ -0,0 +1,5 @@
+public class MyChessAndGoGame {
+	public static void main(String[] args) {
+		Game game =new Game();	
+	}
+}
---
+++ PlayChess/src/Position.java
@@ -0,0 +1,10 @@
+public class Position {
+	public char[] pos;
+	public char[] getPos() {
+		return pos;
+	}
+	public void setPos(char[] pos) {
+		this.pos = pos;
+	} 
+	
+}
